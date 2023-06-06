55734c41e9465de4513186aa3b88f6f1f77c9478
Wang Liao <wliao7@ucsc.edu>
2023-06-05 17:42:47 -0700

    slide scroll implemented/loop spaceship finished

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -12,6 +12,8 @@ class Scene1 extends Phaser.Scene
         //define scene variables
         this.sceneAreaSize = 320;
         this.daveSpeed = 150;
+        this.scrollDuration = 400;
+        this.scrollStyle = 'Quad';
 
         //define tilemap
         const map = this.add.tilemap("scene1_tileMapJSON");
@@ -31,7 +33,7 @@ class Scene1 extends Phaser.Scene
         //set main camera and follow player
         this.mainCam = this.cameras.main;
         this.mainCam.setBounds(0, 0, this.sceneAreaSize * 3, this.sceneAreaSize * 3);
-        this.mainCam.startFollow(this.dave, true);
+        this.mainCam.centerOn(this.dave.x, this.dave.y);
 
         //set collision
         wallLayer.setCollisionByProperty({collides: true});
@@ -51,6 +53,7 @@ class Scene1 extends Phaser.Scene
     update()
     {
         this.daveMove();
+        this.checkCamBounds(this.dave, this.mainCam);
     }
 
     daveMove()
@@ -75,5 +78,89 @@ class Scene1 extends Phaser.Scene
         }
         this.direction.normalize();
         this.dave.setVelocity(this.daveSpeed * this.direction.x, this.daveSpeed * this.direction.y);
+    }
+
+    // uses a camera pan and object tween to create "slideshow" scrolling
+    // assumes object has 0.5 origin
+    // calculations in camera pans need to use cam.centerX/centerY b/c pans are relative to camera center
+    checkCamBounds(obj, cam) {
+        if(obj.x + obj.width/2 > cam.width + cam.scrollX) {
+            // PLAYER HITS RIGHT EDGE (SCROLL R->L)
+            // lock player
+            obj.scrollLock = true;
+            // tween player
+            this.tweens.add({
+                targets: obj,
+                duration: this.scrollDuration,
+                ease: this.scrollStyle,
+                x: { from: obj.x, to: obj.x + obj.width },
+                onComplete: function() {
+                    obj.scrollLock = false; // unlock player
+                }
+            });
+            // pan camera
+            cam.pan(cam.scrollX + cam.centerX + cam.width, cam.scrollY + cam.centerY, this.scrollDuration, this.scrollStyle);
+        } else if(obj.x - obj.width/2 < cam.scrollX) {
+            // PLAYER HITS LEFT EDGE (SCROLL L->R)
+            // lock player
+            obj.scrollLock = true;
+            // tween player
+            this.tweens.add({
+                targets: obj,
+                duration: this.scrollDuration,
+                ease: this.scrollStyle,
+                x: { from: obj.x, to: obj.x - obj.width },
+                onComplete: function() {
+                    obj.scrollLock = false; // unlock player
+                }
+            });
+            // pan camera
+            cam.pan(cam.scrollX - cam.centerX, cam.scrollY + cam.centerY, this.scrollDuration, this.scrollStyle);
+        } else if(obj.y + obj.height/2 > cam.height + cam.scrollY) {
+            // PLAYER HITS BOTTOM EDGE (SCROLL BOTTOM -> TOP)
+            // lock player
+            obj.scrollLock = true;
+            //PLAYER HITS NORMAL BUTTOM EDGE
+            // tween player
+            this.tweens.add({
+                targets: obj,
+                duration: this.scrollDuration,
+                ease: this.scrollStyle,
+                y: { from: obj.y, to: obj.y + obj.height },
+                onComplete: function() {
+                    obj.scrollLock = false; // unlock player
+                }
+            });
+            // pan camera
+            cam.pan(cam.scrollX + cam.centerX, cam.scrollY + cam.centerY + cam.height, this.scrollDuration, this.scrollStyle);
+        } else if(obj.y - obj.height/2 < cam.scrollY) {
+            // PLAYER HITS TOP EDGE (SCROLL TOP->BOTTOM)
+            // lock player
+            obj.scrollLock = true;
+            // tween player
+            this.tweens.add({
+                targets: obj,
+                duration: this.scrollDuration,
+                ease: this.scrollStyle,
+                y: { from: obj.y, to: obj.y - obj.height },
+                onComplete: function() {
+                    obj.scrollLock = false; // unlock player
+                }
+            });
+            // pan camera
+            cam.pan(cam.scrollX + cam.centerX, cam.scrollY - cam.centerY, this.scrollDuration, this.scrollStyle);
+        }
+        else if(obj.y - obj.height/2 <= 0)
+        {
+            //PLAYER HITS TOP EDGE OF THE SPACESHIP, MOVE PLAYER TO THE BUTTOM EDGE OF THE SPACESHIP
+            cam.setScroll(cam.scrollX, this.sceneAreaSize * 2);
+            obj.y = cam.scrollY + cam.height - obj.height/2 - 1;
+        }
+        else if(obj.y + obj.height / 2 >= this.sceneAreaSize * 3)
+        {
+            //PLAYER HITS BUTTOM EDGE OF THE SPACESHIP, MOVE PLAYER TO THE TOP EDGE OF THE SPACESHIP
+            cam.setScroll(cam.scrollX, 0);
+            obj.y = cam.scrollY + obj.height/2 + 1;
+        }
     }
 }
