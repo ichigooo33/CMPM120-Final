9f10f2fa9a03b87bc0c9286f7cebe2d7dfe2ebf0
Wang Liao <wliao7@ucsc.edu>
2023-06-13 22:13:36 -0700

    main.js update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-//Wang Liao
+/*
+    Name: Wang Liao
+    Game Name: 2001: A Space Odyssey Discovery One
+
+    Major components of Phaser used in game:
+        1. physics systems
+        2. cameras
+        3. text objects
+        4. tween manager
+        5. tilemaps
+
+    Creative Tilt:
+    
+    I tried my best learning how Professors' examples about DialogueBox and ScrollStyle work.
+    After I fully understood how they worked, I modified them so they work better for the purpose of my game.
+    
+    For example, I used a golbal variable bool to store if the player finishs the dialogue of scene 1.
+    Every time the player moves to a different area (I combined DialogueBox mechanic with the camera scrolling), player can read the dialogue again if the player presses "R".
+    I think it's an important point because the dialogue also tells the player how to progress the game. If the player misses some information in the dialogue, thep player can go back to read it again.
+*/
+
 
 'use strict';
 
